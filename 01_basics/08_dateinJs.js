f7715be1ab67a52eab225bc4c76ce0def1af1781
SyntaxKoder21 <bakshikashish02@gmail.com>
2025-03-28 11:48:06 +0000

    date done

---
+++ 01_basics/08_dateinJs.js
@@ -0,0 +1,33 @@
+// Dates
+
+// we made object ka instance
+let myDate = new Date();
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleDateString());
+
+// console.log(typeof myDate); // Important Question
+
+// // Whatever written like .toString are the methods and since these are methods so, we need to run them by putting (). Finally => .toString()
+
+// // Here, date is represented in array form. So, month starts from 0.
+// let myCreatedDate = new Date(2024, 0, 21) // Month start from 0 in JS
+// console.log(myCreatedDate.toDateString());
+
+// // Here, date is represented using the "YY-MM-DD" format.
+// let myAnotherDate = new Date("2024-01-21")
+// console.log(myAnotherDate.toDateString());
+
+// Here, date is represented using the "MM-DD-YY" format.
+let myCreatedDate = new Date("01-21-2024") // In India, we prefer this method
+console.log(myCreatedDate.toDateString());
+
+// 11 mins
+
+
+
+
+
+
