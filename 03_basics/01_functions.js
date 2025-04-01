1c7321868c8de92f1cd67a33124a098c4842f27c
SyntaxKoder21 <bakshikashish02@gmail.com>
2025-04-01 12:57:57 +0000

    halfway

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,24 @@
+// cd .. is used to goto the parent directory of the current folder.
+
+// Write a function to print my name
+
+function SayMyName(){ // here, function is a keyword, SayMyName func_name
+    console.log("H");
+    console.log("I");
+    console.log("K");
+    console.log("E");
+    console.log("R");
+}
+
+// SayMyName; // reference of the function. (Mtlb waha function rheta hai)
+
+// SayMyName(); // execution
+
+// Function to add two numbers
+
+function addTwoNumbers (number1, number2){
+    console.log(number1 + number2);
+}
+addTwoNumbers(3,4);
+addTwoNumbers(3,"4"); // Type conversion gonna occur
+// Having a check is very important in functions because we need to see whether numbers are only passed because the function is of addition of two numbers. So, we will see the conditional checks later.
