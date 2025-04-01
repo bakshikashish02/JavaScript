c2d70e8459b370004d151cb9fe572c671b20b6d2
SyntaxKoder21 <bakshikashish02@gmail.com>
2025-04-01 12:03:14 +0000

    JSON talk done

--- 02_basics/05_objectDeStructure.js
+++ 02_basics/05_objectDeStructure.js
@@ -17,8 +17,35 @@ console.log(courseInstructor);
 
 // Suppose name courseInstructor seems quite large for you then we can also rename it - Use method given below
 
+// DeStructuring of objects to get values within the object.
 // DeStructuring of values given below
 const {courseInstructor: instructor} = course
 
 console.log(instructor);
+
+// API - jab aapko apna kaam kisi ke sarr pe dal dena hai, then we use API Eg - login function of the gmail.
+
+// API sends some values from the backend and how you gonna write them earlier, that values used to come in XML structure that was very complex. But these values comes in JSON structure.
+
+// JSON structure (syntax) 
+// JSON file can be of object type, or the array type [Syntax below]
+/*
+{
+    "name": "hitesh sir",
+    "coursename": "JS in hindi",
+    "cost": "free"
+}
+*/
+
+/*
+[
+    {},
+    {},
+    {}
+]
+*/
+
+
+// Let's see how API is called through JSON - BookMarked components.
+// So, basically we want to learn fetch method in the JavaScript. - Will see later.
 
